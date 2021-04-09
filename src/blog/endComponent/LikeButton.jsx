b74d3a300c81da12249d134bc408452c566bc7b5
SUTAKURE <udonnamerou@gmail.com>
2021-04-09 22:59:55 +0900

    関数コンポーネントでライフサイクルを扱う

---
+++ src/blog/endComponent/LikeButton.jsx
@@ -0,0 +1,22 @@
+import React, {useState,useEffect} from "react"
+
+const LikeButton = () => {
+    const [count,counter] = useState(0);
+
+  
+    useEffect( () => {
+        const countUp = () => {counter(count + 1)}
+
+        document.getElementById("counter").addEventListener("click", countUp)
+        if(count >= 10){counter(0)}
+        return () =>{
+            document.getElementById("counter").removeEventListener("click", countUp)
+        }
+    },[count]);
+    
+    return(
+        <button id = {"counter"}>いいね数: {count}</button>
+    )
+};
+
+export default LikeButton
