414de274f935e4ff860eb8a7974a15e436878bb0
SUTAKURE <udonnamerou@gmail.com>
2021-04-09 19:37:58 +0900

    コンポーネントの再利用性について学習

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -4,10 +4,11 @@ import Article from "./Article"
 const Blog = (props) => {
     return (
         <div>
-            <Article />
+            <Article title={"Reactの使い方"} order={1}/>
+            <Article title={"JSXの使い方"} order={2}/>
+            <Article title={"環境構築をしてみよう"} order={3}/>
         </div>
     )
 }
-
 
 export default Blog
