ddb7e4cecb1f061918484768b6aa39b1e635f1c4
SUTAKURE <udonnamerou@gmail.com>
2021-04-09 18:51:57 +0900

    コンポーネントの理解

---
+++ src/Blog.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Article from "./Article"
+
+const Blog = (props) => {
+    return (
+        <div>
+            <Article />
+        </div>
+    )
+}
+
+
+export default Blog
