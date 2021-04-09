0e7c028d1dec484b37a813cb068c28b3198350f8
SUTAKURE <udonnamerou@gmail.com>
2021-04-09 21:59:30 +0900

    ライフサイクルなんとなく理解したい

--- src/blog/Blog.jsx
+++ src/blog/Blog.jsx
@@ -6,21 +6,40 @@ class Blog extends React.Component {
         super(props);
         this.state = {
             isPublished: false,
-            order: 1
+            count: 0
         } 
     }
     
+    componentDidMount() {
+        // ボタンがクリックされたらいいねをカウントアップする
+        document.getElementById("counter").addEventListener("click", this.countUp)
+    }
+
+    componentDidUpdate() {
+        if(this.state.count >= 10){this.setState({count: 0})}
+    }
+
+    componentWillUnmount(){
+        document.getElementById("counter").removeEventListener("click", this.countUp)
+    }
+
     // 公開状態を反転させる関数
     togglePublished = () => {
-        this.setState( {
-            isPublished: !this.state.isPublished
-        })
+        this.setState({ isPublished: !this.state.isPublished })
     };
+
+    countUp = () => {
+        this.setState({ count: this.state.count + 1})
+    }
 
     render() {
     return (
         <div>
-            <Article title={"Reactの使い方"} isPublished={this.state.isPublished} toggle={() => this.togglePublished()}/>
+            <Article title = {"Reactの使い方"} 
+            isPublished = {this.state.isPublished}
+            toggle = {() => this.togglePublished()}
+            count = {this.state.count}
+            />
         </div>
     )}
 }
